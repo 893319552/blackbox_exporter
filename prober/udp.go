efa8fa87596b6d9c3d2d248095865e996d569b25
893319552 <130732854+893319552@users.noreply.github.com>
2025-02-24 10:18:54 +0800

    Update udp.go
    Signed-off-by: 893319552 <130732854+893319552@users.noreply.github.com>

--- prober/udp.go
+++ prober/udp.go
@@ -36,7 +36,7 @@ func dialUDP(ctx context.Context, target string, module config.Module, registry 
 		return nil, err
 	}
 
-	ip, err := chooseProtocol(module.UDP.PreferredIPProtocol, targetAddress, registry, logger)
+	ip, err := chooseProtocol(module.UDP.IPProtocol, targetAddress, registry, logger)
 	if err != nil {
 		level.Error(logger).Log("msg", "Error resolving address", "err", err)
 		return nil, err
